75448850c3f0c1f1c1035046970714ac44d14f83
于涛 <shiluodegucheng@126.com>
2015-04-08 20:38:46 +0800

    fsdafas

--- 16houtaipesong/dingdanqueren/js/main_1.js
+++ 16houtaipesong/dingdanqueren/js/main_1.js
@@ -5,20 +5,31 @@ var myScroll,
  * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
  */
 function pullDownAction(c, d) {
+	setTimeout(function() { // <-- Simulate network congestion, remove setTimeout from production!
+		var el, dt, dd, i;
+		el = document.getElementById(c);
+		for (i = 0; i < 3; i++) {
+			dt = document.createElement('dt');
+			dd = document.createElement('dd');
+			dt.innerHTML = '<div class=\"left\"><label>订单号</label><div>我是订单号</div></div><div class=\"right\"><a href=\"javascript\:\;\" class=\"blue_btn ui-link\">配送完成</a></div>';
+			dd.innerHTML = '<div><p><label>收货人：</label><span>INCAKE</span></p><p><label>手机/电话：</label><span>13584625963</span></p></div><p><label>收货时间：</label><span>2015.03.04 09:30~10:30</span></p><p><label>商品信息：</label><span>女王芝士 1.5磅&chi;1<br />女王芝士 1.5磅&chi;1</span></p>';
+			el.insertBefore(dd, el.childNodes[0]);
+			el.insertBefore(dt, el.childNodes[0]);
+		}
+		//						_refresh();
+		d.refresh(); //数据加载完成后，调用界面更新方法   Remember to refresh when contents are loaded (ie: on ajax completion)
+	}, 1000); // <-- Simulate network congestion, remove setTimeout from production!
+}
+
+/**
+ * 滚动翻页 （自定义实现此方法）
+ * myScroll.refresh();		// 数据加载完成后，调用界面更新方法
+ */
+
+function pullUpAction(d) {
 		setTimeout(function() { // <-- Simulate network congestion, remove setTimeout from production!
-			var el, dt,dd, i;
-			el = document.getElementById(c);
-			for (i = 0; i < 3; i++) {
-				dt = document.createElement('dt');
-				dd = document.createElement('dd');
-				dt.innerHTML = '<div class=\"left\"><label>订单号</label><div>我是订单号</div></div><div class=\"right\"><a href=\"javascript\:\;\" class=\"blue_btn ui-link\">配送完成</a></div>';
-				dd.innerHTML ='<div><p><label>收货人：</label><span>INCAKE</span></p><p><label>手机/电话：</label><span>13584625963</span></p></div><p><label>收货时间：</label><span>2015.03.04 09:30~10:30</span></p><p><label>商品信息：</label><span>女王芝士 1.5磅&chi;1<br />女王芝士 1.5磅&chi;1</span></p>';
-				el.insertBefore(dd, el.childNodes[0]);
-				el.insertBefore(dt, el.childNodes[0]);
-			}
-			//						_refresh();
-			d.refresh(); //数据加载完成后，调用界面更新方法   Remember to refresh when contents are loaded (ie: on ajax completion)
-		}, 1000); // <-- Simulate network congestion, remove setTimeout from production!
+			d.refresh(); // 数据加载完成后，调用界面更新方法 Remember to refresh when contents are loaded (ie: on ajax completion)
+		}, 100); // <-- Simulate network congestion, remove setTimeout from production!
 	}
 	/**
 	 * 初始化iScroll控件
@@ -27,6 +38,8 @@ function pullDownAction(c, d) {
 function loaded(a, b, c, d) {
 		var pullDownEl = document.getElementById(b);
 		var pullDownOffset = pullDownEl.offsetHeight;
+		var pullUpEl = d == 'scroller_1' ? document.getElementById('pullUp_1') : document.getElementById('pullUp_2');
+		var pullUpOffset = pullUpEl.offsetHeight;
 		d = new iScroll(a, {
 			scrollbarClass: 'myScrollbar',
 			/* 重要样式 */
@@ -48,6 +61,12 @@ function loaded(a, b, c, d) {
 					pullDownEl.className = 'pullDown';
 					pullDownEl.querySelector('.pullDownLabel').innerHTML = '下拉刷新...';
 					this.minScrollY = -pullDownOffset;
+				} else if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'flip';
+					this.maxScrollY = this.maxScrollY;
+				} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+					pullUpEl.className = '';
+					this.maxScrollY = pullUpOffset;
 				}
 			},
 			onScrollEnd: function() {
@@ -55,6 +74,9 @@ function loaded(a, b, c, d) {
 					pullDownEl.className = 'loading pullDown';
 					pullDownEl.querySelector('.pullDownLabel').innerHTML = '加载中...';
 					pullDownAction(c, d); // Execute custom function (ajax call?)
+				} else if (pullUpEl.className.match('flip')) {
+					pullUpEl.className = 'loading';
+					pullUpAction(d); // Execute custom function (ajax call?)
 				}
 			}
 		});
